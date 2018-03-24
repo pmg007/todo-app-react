6a3d778c4bd5c1d134a4d0186d045e61e9fb1855
pmg007 <pragammgandhi@gmail.com>
2018-03-24 01:07:08 -0400

    replaced class by a const function in SingleTodo.js

--- src/SingleTodo.js
+++ src/SingleTodo.js
@@ -1,13 +1,19 @@
 import React, {Component} from "react"
-class SingleTodo extends Component {
-    constructor() {
-        super();
-    }
+// class SingleTodo extends Component {
+//     constructor() {
+//         super();
+//     }
 
-    render() {
-        return (
-            <li>{this.props.todo}<button onClick={this.props.delete}>X</button></li>
-        );
-    }
+//     render() {
+//         return (
+//             <li>{this.props.todo}<button onClick={this.props.delete}>X</button></li>
+//         );
+//     }
+// }
+
+const SingleTodo = props => {
+    return (
+        <li>{props.todo}<button onClick={props.delete}>X</button></li>
+    );
 }
 export default SingleTodo;
