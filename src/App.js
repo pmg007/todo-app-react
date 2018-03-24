586952b97dcf0470cc102ede3e9a0c7938c2f481
pmg007 <pragammgandhi@gmail.com>
2018-03-24 00:55:10 -0400

    added SingleTodo.js and functionality to delete a todo item from the list

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import SingleTodo from "./SingleTodo";
 
 class App extends Component {
   constructor() {
@@ -21,10 +22,17 @@ class App extends Component {
     this.setState({todos: todosCopy, currentTodo: ""})
   }
 
+  deleteTodo = i => {
+    let todosCopy = this.state.todos.slice();
+    todosCopy.splice(i,1);
+    this.setState({todos:todosCopy});
+  }
+
   render() {
     let  bulletedTools = this.state.todos.map((e,i) => {
       return ( 
-        <li key={i}>{e} <button>X</button></li>
+        //<li key={i}>{e} <button>X</button></li>
+        <SingleTodo todo ={e} delete={()=>this.deleteTodo(i)}/>
       );
 
     });
